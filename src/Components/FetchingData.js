544765e3153dc563d0f05b6e2721a208fe77750a
Staff Asia <mahbuburrahman@Ismail-Hossain.local>
2022-02-24 11:41:39 +0600

    Data loading added and error control added to reducer

--- src/Components/FetchingData.js
+++ src/Components/FetchingData.js
@@ -3,54 +3,86 @@ import React, { useEffect, useReducer } from 'react';
 
 const initialState = {
     users: [],
-    album: []
+    album: [],
+    loading: true,
+    error1: '',
+    error2: '',
 };
 
 const reducer = (state, action) => {
     switch (action.type) {
         case 'LOADUSER':
-            return { ...state, users: action.setData };
+            return { ...state, loading: false, users: action.payload, error1: '', };
         case 'LOADALBUM':
-            return { ...state, album: action.setData };
-
+            return { ...state, loading: false, album: action.payload, error2: '', };
+        case 'ERROR1':
+            return { ...state, users: [], error1: action.payload }
+        case 'ERROR2':
+            return { ...state, album: [], error2: action.payload }
         default:
-            return initialState
+            return state;
     }
 }
 
 
 const FetchingData = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
-
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(data => dispatch({
                 type: 'LOADUSER',
-                setData: data
-            }))
+                payload: data
+            })
+            )
+            .catch(error => dispatch({ type: 'ERROR1', payload: 'user not found.something went wrong' }))
     }, [])
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/albums')
             .then(response => response.json())
             .then(data => dispatch({
                 type: 'LOADALBUM',
-                setData: data
-            }))
+                payload: data
+            })
+            )
+            .catch(error => dispatch({ type: 'ERROR2', payload: 'album not found.something went wrong' }))
     }, [])
-
-
     return (
         <div style={{ textAlign: 'center' }}>
             <h2>Data Fetching</h2>
-            {
-                state.users?.map(user => (
-                    <div key={user.id}>
+            <br />
+            <h3>Users</h3>
+            {state.error1 && <h4 style={{ color: 'red' }}>{state.error1}</h4>}
+            {state.loading ? "Loading..." :
+                <>
+                    {
+                        state?.users?.map(user => (
+                            <div key={user.id}>
 
-                        <h4>{user.name}</h4>
-                    </div>
-                ))
+                                <h4>{user.name}</h4>
+                            </div>
+                        ))
+                    }
+                </>
+
+
             }
+
+            <br />
+            <h3>Album</h3>
+            {state.error2 && <h4 style={{ color: 'red' }}>{state.error2}</h4>}
+            {state.loading ? 'Loading...' :
+
+                <>
+                    {
+                        state.album?.slice(0, 10).map(element => (
+                            <h4 key={element.id}>{element.title}</h4>
+                        )
+                        )}
+                </>
+
+            }
+
         </div>
     );
 };
